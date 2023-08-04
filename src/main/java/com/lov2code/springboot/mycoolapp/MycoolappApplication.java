b83a045a257d66f7048a02d133551ba849d62169
qasim-leo <qasim@jfreaks.com>
2023-08-04 16:48:19 +0500

    Overriden files

---
+++ src/main/java/com/lov2code/springboot/mycoolapp/MycoolappApplication.java
@@ -0,0 +1,13 @@
+package com.lov2code.springboot.mycoolapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycoolappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycoolappApplication.class, args);
+	}
+//	Hey
+}
