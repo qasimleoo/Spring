f727a4fea4069a54a78e076a3b53bb8eb3525ee3
qasim-leo <qasim@jfreaks.com>
2023-08-08 13:11:03 +0500

    .properties in spring

--- src/main/java/com/lov2code/springboot/mycoolapp/rest/restExample.java
+++ src/main/java/com/lov2code/springboot/mycoolapp/rest/restExample.java
@@ -1,5 +1,6 @@
 package com.lov2code.springboot.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +25,16 @@ public class restExample {
     @GetMapping("fibonocci")
     public String getFibnocci(){
         return "Fibnocci of 5 is 3";
+    }
+
+//    Accessing resources/__.properties (Custom  properties)
+    @Value("${coach.name}")
+    private String name;
+    @Value("${coach.age}")
+    private Integer age;
+    @GetMapping("Injection")
+    public String getProperties(){
+        return "Coach name is " + name + " and his age is " + age;
     }
 
 }
